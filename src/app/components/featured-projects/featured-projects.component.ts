0064e26c68ef52886e53b99e3ca1e05388e45dc9
Blockost <simon.espigole@gmail.com>
2019-01-20 14:56:06 -0500

    Moved featured projects inside an animated carousel

--- src/app/components/featured-projects/featured-projects.component.ts
+++ src/app/components/featured-projects/featured-projects.component.ts
@@ -9,18 +9,27 @@ export class FeaturedProjectsComponent implements OnInit {
   projects = [
     {
       name: 'Audio Visualization',
+      description:
+        "Simple sound spectrum visualizer using Three.js. Note: there's a pretty cool music playing!",
       img: '/assets/img/featured-projects/sudoku-game.png',
-      url: 'https://sudoku.blockost.com'
+      url: 'https://sudoku.blockost.com',
+      repoUrl: 'https://github.com/Blockost/AudioVisualization'
     },
     {
       name: 'Sudoku Game',
+      description:
+        'In-browser single-player Sudoku game board generator and solver with random generation using recursion and backtracking',
       img: '/assets/img/featured-projects/sudoku-game.png',
-      url: 'https://sudoku.blockost.com'
+      url: 'https://sudoku.blockost.com',
+      repoUrl: 'https://github.com/Blockost/sudoku'
     },
     {
       name: 'Url Shortener',
+      description:
+        'Shorten long, unfriendly links in a matter of seconds using this simple link management platform.',
       img: '/assets/img/featured-projects/url-shortener.png',
-      url: 'https://sudoku.blockost.com'
+      url: 'https://sudoku.blockost.com',
+      repoUrl: 'https://github.com/Blockost/url-shortener'
     }
   ];
 
