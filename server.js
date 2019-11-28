2b5238efbefdb448a78df0b9db646cf6ff038322
Blockost <simon.espigole@gmail.com>
2019-11-28 18:42:19 +0100

    config: requires server port to be defined in order to start app on prod

--- server.js
+++ server.js
@@ -16,7 +16,11 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/dist/index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.APP_PORTFOLIO_PORT;
+if (!PORT) {
+  throw new Error('PORT is not defined');
+}
+
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}.`);
 });
